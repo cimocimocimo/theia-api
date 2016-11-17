de83e0f9c3621ff9ad0154e36ce2c4cbf350b6c8
Aaron Cimolini <aaron@cimolini.com>
2016-11-16 21:26:25 -0800

    Fixes database settings for production.

--- settings/production.py
+++ settings/production.py
@@ -15,14 +15,13 @@ ALLOWED_HOSTS = [
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['PROD_RDS_DB_NAME'],
-            'USER': os.environ['PROD_RDS_USERNAME'],
-            'PASSWORD': os.environ['PROD_RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['PROD_RDS_DB_NAME'],
+        'USER': os.environ['PROD_RDS_USERNAME'],
+        'PASSWORD': os.environ['PROD_RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
+}
