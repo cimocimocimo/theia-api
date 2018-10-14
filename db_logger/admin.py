c6a93b36968ea2face9638fa45017a116c597f48
Aaron Cimolini <aaron@cimolini.com>
2018-10-13 23:24:02 -0700

    Adds admin page for DB Logger.

--- db_logger/admin.py
+++ db_logger/admin.py
@@ -1,8 +1,42 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 
 from .models import DBLogEntry
 
 
 @admin.register(DBLogEntry)
 class DBLogEntryAdmin(admin.ModelAdmin):
-    pass
+
+    # Prevent adding, changing, or deleting Model objects manually.
+    def has_add_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    fields = (
+        'level',
+        'entry_date',
+        'message',
+        'import_job_link',)
+    list_display = (
+        '__str__',
+        'level',
+        'entry_date',
+        'message',
+        'import_job_link',)
+    list_filter = (
+        'level',
+        'entry_date',)
+
+    def import_job_link(self, obj):
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse(
+                'admin:dropbox_import_importjob_change',
+                args=(obj.import_job.pk,)),
+            obj.import_job,)
+    import_job_link.short_description = 'Import Job'
+
